785db0c13f1069ae9034343661384f707b6a01a9
Kevin <kh925063@gmail.com>
2026-06-10 13:03:38 -0600

    agrenado .env y revisando que se conecte a SQL

--- dbconexion.py
+++ dbconexion.py
@@ -1,9 +1,23 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="tu_usuario",
-        password="tu_password",
-        database="inventario"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
+
+
+if __name__ == "__main__":
+    try:
+        conn = get_connection()
+        print("Conexion exitosa a MySQL")
+        conn.close()
+    except Exception as e:
+        print(" Error en la conexionn:", e)
