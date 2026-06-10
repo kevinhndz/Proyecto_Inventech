53959b2331ed3d8553abe81831bbcceaa9cd5511
Kevin <kh925063@gmail.com>
2026-06-10 16:03:05 -0600

    agrgue la funcion ver materiales, imorte dos nuevas librerias para mostras mensaje en pntalla

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+from tkinter import messagebox
+from dbconexion import get_connection
 
 ctk.set_appearance_mode("System")   # usa el modo claro/oscuro del sistema
 ctk.set_default_color_theme("blue") # define el color principal
@@ -12,9 +14,27 @@ ventana.geometry("600x400")
 label = ctk.CTkLabel(ventana, text="Inventech - Gestion de Materiales", font=("Arial", 20))
 label.pack(pady=20)
 
+def ver_materiales():
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT nombre, cantidad, categoria FROM materiales")
+        registros = cursor.fetchall()
+        conn.close()
+
+        if registros:
+            texto = "\n".join([f"{nombre} - {cantidad} ({categoria})" for nombre, cantidad, categoria in registros])
+            messagebox.showinfo("Materiales", texto)
+        else:
+            messagebox.showinfo("Materiales", "No hay registros en la tabla.")
+    except Exception as e:
+        messagebox.showerror("Error", f"Ocurrio un problema: {e}")
+
+
 # Boton para ver materiales
-boton_ver = ctk.CTkButton(ventana, text="Ver materiales")
+boton_ver = ctk.CTkButton(ventana, text="Ver materiales", command=ver_materiales)
 boton_ver.pack(pady=10)
+
 
 # Boton para agregar material
 boton_agregar = ctk.CTkButton(ventana, text="Agregar material")
